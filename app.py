44bf20ae7b8759d121ec5f9c52be0d7d386944bf
ThierryArmant <thierryarmant@gmail.com>
2026-06-03 12:08:49 +0200

    Update app.py ajout ligne 32 "zoom: 0.8; "

--- app.py
+++ app.py
@@ -29,6 +29,7 @@ def inject_glass_theme(img_b64):
         background-position: center !important;
         background-attachment: fixed !important;
         background-repeat: no-repeat !important;
+        zoom: 0.8;
     }}
     """ if img_b64 else """
     [data-testid="stAppViewContainer"] {{
